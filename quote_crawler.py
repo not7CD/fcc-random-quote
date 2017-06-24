3f16af1846c32d50aac5f8617912b2e6259e940e
not7CD <norbertszulc13@gmail.com>
2017-06-24 12:34:08 +0200

    GET every found href

--- quote_crawler.py
+++ quote_crawler.py
@@ -1,13 +1,29 @@
+"""Getting contets of blockquote from all the html files"""
 from bs4 import BeautifulSoup
 import urllib.request
 
-"""Getting contets of blockquote from all the html files"""
 
 
-def get_blockquote_from_file(file):
+def get_blockquote_from_html(quote_html):
     """Extracts all blockquotes from given file.
         Returns list."""
-    pass
+    soup = BeautifulSoup(quote_html, "html.parser")
+    quote = None
+    try:
+        quote = soup.blockquote.p
+        for span in quote('span'):
+            span.extract()
+
+        print(list(quote.contents).index ('<br/>'))
+
+        # for i, c in enumerate(quote.contents):
+            # print(i, c)
+            # print(type(c.string))
+        # print(quote.contents[2])
+    except AttributeError as e:
+        print(e)
+    return quote
+
 
 
 def main(index_html):
@@ -15,9 +31,15 @@ def main(index_html):
     # print(index_html)
     soup = BeautifulSoup(index_html, "html.parser")
     for link in soup.find_all('a'):
-        print(link)
+        try:
+            HTML = urllib.request.urlopen("http://www.diveintopython3.net/" + link['href']).read()
+            quote = get_blockquote_from_html(HTML)
+            print(quote)
+        except urllib.error.HTTPError as e:
+            print(e)
+
 
 
 if __name__ == '__main__':
-    html = urllib.request.urlopen("http://www.diveintopython3.net/").read()
-    main(html)
+    HTML = urllib.request.urlopen("http://www.diveintopython3.net/").read()
+    main(HTML)
