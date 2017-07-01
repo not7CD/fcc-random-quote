a687d3cc43edde1643a42e69351ac756a3aea721
not7CD <norbertszulc13@gmail.com>
2017-07-01 21:40:35 +0200

    Separate code to a fucntion

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+rand_quote = function(quotes) {
+  var i = Math.floor(Math.random() * quotes.length);
+  $("#quote").html(quotes[i].quote);
+  $("#author").html(quotes[i].author);
+};
 
 $(document).ready(function() {
   var json_quotes = [];
   $.getJSON( "quotes.json", function(data) {
-    json_quotes = data["quotes"];
+    json_quotes = data.quotes;
+    rand_quote(json_quotes);
   });
-  $("#getQuote").on("click", function() {
-    var i = Math.floor(Math.random() * json_quotes.length);
-    $("#quote").html(json_quotes[i].quote);
-    $("#author").html(json_quotes[i].author);
-  });
+
+  $("#getQuote").on("click", rand_quote(json_quotes));
 });
