0e7a6b124b216c99a41d223bca0b3577c08e63d0
not7CD <norbertszulc13@gmail.com>
2017-07-01 21:26:09 +0200

    GET quotes from local JSON

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-var parsed_json_file = [{
-    cite: 'http://www.diveintopython3.net/whats-new.html',
-    quote: 'Isn’t this where we came in?',
-    author: 'Pink Floyd, The Wall'
-  },
-  {
-    quote: 'Certitude is not the test of certainty. We have been cocksure of many things that were not so.',
-    author: 'Oliver Wendell Holmes, Jr.'
-  }
-];
 
 $(document).ready(function() {
+  var json_quotes = [];
+  $.getJSON( "quotes.json", function(data) {
+    json_quotes = data["quotes"];
+  });
   $("#getQuote").on("click", function() {
-    var i = Math.floor(Math.random() * parsed_json_file.length);
-    $("#quote").html(parsed_json_file[i].quote);
-    $("#author").html(parsed_json_file[i].author);
+    var i = Math.floor(Math.random() * json_quotes.length);
+    $("#quote").html(json_quotes[i].quote);
+    $("#author").html(json_quotes[i].author);
   });
 });
